989b139be9eab75b62b5af7a65393c15ff652365
Hachiouji <ale_code96@hotmail.com>
2017-05-12 10:40:34 -0600

    Avance
    Aunque aparezcan muchos nuevos archivos, sólo se agregó lo de que
    aparezca el segundo valor de la carta

--- app/src/main/java/com/example/ale/aplicacionmd/showFlashcards.java
+++ app/src/main/java/com/example/ale/aplicacionmd/showFlashcards.java
@@ -1,13 +1,26 @@
 package com.example.ale.aplicacionmd;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+
 
 import org.w3c.dom.Text;
 
@@ -17,42 +30,25 @@ import org.w3c.dom.Text;
 
 public class showFlashcards extends AppCompatActivity
 {
-
-    public CardView itemFC;
     public TextView itemCardText;
     public int cont_type=2;
     public int cont_position=0;
     public int sizeSet;
     public String texto;
-
     FlashcardsDBHelper opener = new FlashcardsDBHelper(this);
 
-
-    //final int position = getIntent().getExtras("Nombre");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ind_cards);
 
-
-
         final String nombreSet = getIntent().getExtras().getString("Nombre");
         texto = opener.obtenerLados(cont_type, cont_position, nombreSet);
         sizeSet=opener.sizeINT(nombreSet, EsquemaFlashcard.EntradaFlashcard.TABLE_NAME);
 
-
-
         itemCardText = (TextView) findViewById(R.id.card_text);
-
         itemCardText.setText(texto);
-
-
-
-        //Cadena.setText(texto);
-
-        //itemCardText = (TextView) findViewById(R.id.card_text);
-        //OnSwipeTouchListener MovementsOnScreen = new OnSwipeTouchListener(this);
 
         CardView flashcard = (CardView) findViewById(R.id.card_view);
         flashcard.setOnTouchListener(new OnSwipeTouchListener(showFlashcards.this)
@@ -61,7 +57,7 @@ public class showFlashcards extends AppCompatActivity
             //Izquierda
             public void onSwipeRight()
             {
-
+                cont_type=2;
                 if (cont_position==0)
                 {
                     cont_position = sizeSet-1;
@@ -80,7 +76,7 @@ public class showFlashcards extends AppCompatActivity
             //Derecha
             public void onSwipeLeft()
             {
-
+                cont_type=2;
                 if (cont_position + 1 == sizeSet)
                 {
                     cont_position = 0;
@@ -95,40 +91,47 @@ public class showFlashcards extends AppCompatActivity
 
             }
 
-        });
 
-        /*
-        flashcard.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
+            //Se realiza de abajo hacia arriba
+            public void onSwipeTop()
             {
-
-                Snackbar.make(view, "Click detected on item", Snackbar.LENGTH_LONG).show();
-
                 if (cont_type == 2)
                 {
                     cont_type = 3;
-                    notify();
                 }
                 else
                 {
                     if (cont_type == 3)
                     {
                         cont_type = 2;
-                        notify();
                     }
                 }
 
+                texto = opener.obtenerLados(cont_type, cont_position, nombreSet);
+                itemCardText.setText(texto);
             }
 
+
+            //Se realiza de arriba hacia abajo
+            public void onSwipeBottom()
+            {
+                if (cont_type == 2)
+                {
+                    cont_type = 3;
+                }
+                else
+                {
+                    if (cont_type == 3)
+                    {
+                        cont_type = 2;
+                    }
+                }
+                texto = opener.obtenerLados(cont_type, cont_position, nombreSet);
+                itemCardText.setText(texto);
+            }
         });
-        */
     }
 
-    public void ClickFlashcard (View view)
-    {
-        Snackbar.make(view, "Click detected on item", Snackbar.LENGTH_LONG).show();
-    }
 
 }
+
---
+++ app/src/main/java/com/example/ale/aplicacionmd/textFrontSideFC.java
@@ -0,0 +1,12 @@
+package com.example.ale.aplicacionmd;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ale_c on 5/12/2017.
+ */
+
+public class textFrontSideFC extends AppCompatActivity
+{
+
+}
